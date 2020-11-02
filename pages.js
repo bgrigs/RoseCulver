5941f7c43573c923c09a590fb87d0705f39449fa
Bethany Grigsby <grigsby.bethany@gmail.com>
2020-11-01 21:18:36 -0800

    add background-color transitions

--- pages.js
+++ pages.js
@@ -3,16 +3,21 @@ let pageNumber = 0;
 
 //Content for pages 
 const pages = [
-    {copy: "a Brooklyn-based graphic designer"},
-    {copy: "Kanye West's biggest fan"},
-    {copy: "looking for a job at the start of October"},
-    {copy: `letting you <a href="pdf.pdf">download her PDF</a>`}
+    {copy: "a Brooklyn-based graphic designer", background: '#edc7a9', circle: '#3e78ed'},
+    {copy: "Kanye West's biggest fan", background: '#a1fffe', 
+    circle: '#e34a47'},
+    {copy: "looking for a job at the start of October", background: '#d3c7f3',
+    circle: '#f7fe00'},
+    {copy: `letting you <a href="pdf.pdf">download her PDF</a>`, background: '#faffb8',
+    circle: '#b472e6'}
 ]
 
 //Pick the relevant tags
 const nextTag = document.querySelector(".next");
 const previousTag = document.querySelector(".prev");
 const outputTag = document.querySelector("h2");
+const circleTag = document.querySelector('.circle');
+const bodyTag = document.querySelector('body');
 
 //Next function
 const next = function() {
@@ -35,6 +40,8 @@ const previous = function() {
 //Update section's content and style
 const updateSection = function() {
     outputTag.innerHTML = pages[pageNumber].copy;
+    circleTag.style.backgroundColor = pages[pageNumber].circle;
+    bodyTag.style.backgroundColor= pages[pageNumber].background;
 }
 
 //On click of nextTag, run this
@@ -46,10 +53,6 @@ nextTag.addEventListener('click', function() {
 previousTag.addEventListener('click', function() {
     previous();
 }) 
-
-
-
-
 
 
 // Random
